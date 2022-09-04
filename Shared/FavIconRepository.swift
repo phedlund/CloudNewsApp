a0af1b12ae7a7445feb6dc0ef5042a1c86128121
Peter Hedlund <support@pbh.dev>
2022-09-04 16:53:53 -0700

    Solidify favicon code

--- Shared/FavIconRepository.swift
+++ Shared/FavIconRepository.swift
@@ -7,17 +7,17 @@
 
 import Combine
 import Foundation
+import Kingfisher
 
-struct NodeIcon: Equatable {
-    var nodeType: NodeType
-    var icon: SystemImage
+enum FetchError: Error {
+    case noImage
 }
 
 @MainActor
 class FavIconRepository: NSObject, ObservableObject {
     var icons = CurrentValueSubject<[NodeType: String], Never>([:])
 
-    private let favIconValidator = FavIconValidator()
+    private let validSchemas = ["http", "https", "file"]
     private let syncPublisher = NotificationCenter.default.publisher(for: .syncComplete, object: nil).eraseToAnyPublisher()
     private var cancellables = Set<AnyCancellable>()
 
@@ -29,7 +29,7 @@ class FavIconRepository: NSObject, ObservableObject {
             .sink { [weak self] _ in
                 Task {
                     do {
-                        try await self?.favIconValidator.fetch()
+                        try await self?.fetch()
                     } catch { }
                 }
                 self?.update()
@@ -54,4 +54,52 @@ class FavIconRepository: NSObject, ObservableObject {
             }
         }
     }
+
+    private func fetch() async throws {
+        if let feeds = CDFeed.all() {
+            for feed in feeds {
+                if let link = feed.faviconLink,
+                   let url = URL(string: link),
+                   let scheme = url.scheme,
+                   validSchemas.contains(scheme) {
+                    do {
+                        try await validateImageUrl(from: url, feed: feed)
+                    } catch let error {
+                        if error as? FetchError == FetchError.noImage {
+                            if let feedUrl = URL(string: feed.link ?? "data:null"),
+                               let host = feedUrl.host,
+                               let url = URL(string: "https://icons.duckduckgo.com/ip3/\(host).ico") {
+                                do {
+                                    try await validateImageUrl(from: url, feed: feed)
+                                } catch { }
+                            }
+                        }
+                    }
+                } else {
+                    if let feedUrl = URL(string: feed.link ?? "data:null"),
+                       let host = feedUrl.host,
+                       let url = URL(string: "https://icons.duckduckgo.com/ip3/\(host).ico") {
+                        do {
+                            try await validateImageUrl(from: url, feed: feed)
+                        } catch { }
+                    }
+                }
+            }
+        }
+    }
+
+    private func validateImageUrl(from url: URL, feed: CDFeed) async throws {
+        let request = URLRequest.init(url: url)
+        let (_, response) = try await URLSession.shared.data(for: request)
+        if let httpResponse = response as? HTTPURLResponse {
+            switch httpResponse.statusCode {
+            case 200:
+                icons.value[.feed(id: feed.id)] = url.absoluteString
+                try await CDFeed.addFavIconLinkResolved(feed: feed, link: url.absoluteString)
+            default:
+                throw FetchError.noImage
+            }
+        }
+    }
+
 }
