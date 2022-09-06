5197a03725c8c7d52fa8cc9f0217cdc0e980c5d5
Peter Hedlund <support@pbh.dev>
2022-09-05 17:01:36 -0700

    Use Grid instead of stacks

--- Shared/ArticleSettingsView.swift
+++ Shared/ArticleSettingsView.swift
@@ -31,131 +31,112 @@ struct ArticleSettingsView: View {
     let buttonWidth = 100.0
 
     var body: some View {
-        HStack(alignment: .center) {
-            Spacer()
-            VStack(alignment: .center) {
-                HStack(spacing: 15) {
-                    let isUnRead = item.unread
-                    let isStarred = item.starred
-                    Button {
-                        Task {
-                            try? await NewsManager.shared.markRead(items: [item], unread: !isUnRead)
-                        }
-                    } label: {
-                        Label {
-                            Text(isUnRead ? "Read" : "Unread")
-                        } icon: {
-                            Image(systemName: isUnRead ? "eye" : "eye.slash")
-                                .imageScale(.large)
-                        }
-                        .labelStyle(.iconOnly)
-                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                        .contentShape(Rectangle())
+        Grid(alignment: .center, horizontalSpacing: 15, verticalSpacing: 20) {
+            GridRow {
+                let isUnRead = item.unread
+                let isStarred = item.starred
+                Button {
+                    Task {
+                        try? await NewsManager.shared.markRead(items: [item], unread: !isUnRead)
                     }
-                    .buttonStyle(.bordered)
-                    Button {
-                        Task {
-                            try? await NewsManager.shared.markStarred(item: item, starred: !isStarred)
-                        }
-                    } label: {
-                        Label {
-                            Text(isStarred ? "Unstar" : "Star")
-                        } icon: {
-                            Image(systemName: isStarred ? "star" : "star.fill")
-                                .imageScale(.large)
-                        }
-                        .labelStyle(.iconOnly)
-                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                        .contentShape(Rectangle())
+                } label: {
+                    Label {
+                        Text(isUnRead ? "Read" : "Unread")
+                    } icon: {
+                        Image(systemName: isUnRead ? "eye" : "eye.slash")
+                            .imageScale(.large)
                     }
-                    .buttonStyle(.bordered)
+                    .labelStyle(.iconOnly)
+                    .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                    .contentShape(Rectangle())
                 }
-                .padding(EdgeInsets(top: 15, leading: 15, bottom: 0, trailing: 15))
-                .fixedSize()
-                HStack(spacing: 15) {
-                    Button {
-                        if settings.fontSize > ArticleSettingsConstants.minFontSize {
-                            settings.fontSize -= 1
-                            print("Font size \(settings.fontSize)")
-                        }
-                    } label: {
-                        Image(systemName: "textformat.size.smaller")
-                            .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
+                Button {
+                    Task {
+                        try? await NewsManager.shared.markStarred(item: item, starred: !isStarred)
                     }
-                    .buttonStyle(.bordered)
-                    Button {
-                        if settings.fontSize < ArticleSettingsConstants.maxFontSize {
-                            settings.fontSize += 1
-                            print("Font size \(settings.fontSize)")
-                        }
-                    } label: {
-                        Image(systemName: "textformat.size.larger")
+                } label: {
+                    Label {
+                        Text(isStarred ? "Unstar" : "Star")
+                    } icon: {
+                        Image(systemName: isStarred ? "star" : "star.fill")
                             .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
                     }
-                    .buttonStyle(.bordered)
+                    .labelStyle(.iconOnly)
+                    .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                    .contentShape(Rectangle())
                 }
-                .padding(EdgeInsets(top: 15, leading: 15, bottom: 0, trailing: 15))
-                .fixedSize()
-                HStack(spacing: 15) {
-                    Button {
-                        if settings.lineHeight > ArticleSettingsConstants.minLineHeight {
-                            settings.lineHeight -= 0.2
-                        }
-                    } label: {
-                        Image(systemName: "line.3.horizontal")
-                            .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
-                    }
-                    .buttonStyle(.bordered)
-                    Button {
-                        if settings.lineHeight < ArticleSettingsConstants.maxLineHeight {
-                            settings.lineHeight += 0.2
-                        }
-                    } label: {
-                        Image("custom.line.3.horizontal")
-                            .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
-                    }
-                    .buttonStyle(.bordered)
-                }
-                .padding(EdgeInsets(top: 15, leading: 15, bottom: 0, trailing: 15))
-                .fixedSize()
-                HStack(spacing: 15) {
-                    Button {
-                        if settings.marginPortrait > ArticleSettingsConstants.minMarginWidth {
-                            settings.marginPortrait -= 5
-                        }
-                    } label: {
-                        Image(systemName: "increase.indent")
-                            .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
-                    }
-                    .buttonStyle(.bordered)
-                    Button {
-                        if settings.marginPortrait < ArticleSettingsConstants.maxMarginWidth {
-                            settings.marginPortrait += 5
-                        }
-                    } label: {
-                        Image(systemName: "decrease.indent")
-                            .imageScale(.large)
-                            .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
-                            .contentShape(Rectangle())
-                    }
-                    .buttonStyle(.bordered)
-                }
-                .padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
-                .fixedSize()
             }
-            .frame(width: 300)
-            Spacer()
+            GridRow {
+                Button {
+                    if settings.fontSize > ArticleSettingsConstants.minFontSize {
+                        settings.fontSize -= 1
+                        print("Font size \(settings.fontSize)")
+                    }
+                } label: {
+                    Image(systemName: "textformat.size.smaller")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+                Button {
+                    if settings.fontSize < ArticleSettingsConstants.maxFontSize {
+                        settings.fontSize += 1
+                        print("Font size \(settings.fontSize)")
+                    }
+                } label: {
+                    Image(systemName: "textformat.size.larger")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+            }
+            GridRow {
+                Button {
+                    if settings.lineHeight > ArticleSettingsConstants.minLineHeight {
+                        settings.lineHeight -= 0.2
+                    }
+                } label: {
+                    Image(systemName: "line.3.horizontal")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+                Button {
+                    if settings.lineHeight < ArticleSettingsConstants.maxLineHeight {
+                        settings.lineHeight += 0.2
+                    }
+                } label: {
+                    Image("custom.line.3.horizontal")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+            }
+            GridRow {
+                Button {
+                    if settings.marginPortrait > ArticleSettingsConstants.minMarginWidth {
+                        settings.marginPortrait -= 5
+                    }
+                } label: {
+                    Image(systemName: "increase.indent")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+                Button {
+                    if settings.marginPortrait < ArticleSettingsConstants.maxMarginWidth {
+                        settings.marginPortrait += 5
+                    }
+                } label: {
+                    Image(systemName: "decrease.indent")
+                        .imageScale(.large)
+                        .frame(minWidth: buttonWidth, maxWidth: .infinity, minHeight: buttonHeight, maxHeight: buttonHeight, alignment: .center)
+                        .contentShape(Rectangle())
+                }
+            }
         }
+        .buttonStyle(.bordered)
+        .padding()
     }
 }
 
