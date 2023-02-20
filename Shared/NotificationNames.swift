f4696080391a470795f842880934e34567985db2
Peter Hedlund <support@pbh.dev>
2023-02-19 16:44:07 -0800

    Clean up notification names

--- Shared/NotificationNames.swift
+++ Shared/NotificationNames.swift
@@ -10,16 +10,9 @@ import Foundation
 
 extension NSNotification.Name {
 
-    public static let syncInitiated = NSNotification.Name("SyncInitiated")
     public static let loginComplete = NSNotification.Name("LoginComplete")
-    public static let folderSync = NSNotification.Name("FolderSync")
-    public static let feedSync = NSNotification.Name("FeedSync")
-    public static let newFolder = NSNotification.Name("NewFolder")
-    public static let newFeed = NSNotification.Name("NewFeed")
     public static let deleteFolder = NSNotification.Name("DeleteFolder")
     public static let renameFolder = NSNotification.Name("RenameFolder")
     public static let deleteFeed = NSNotification.Name("DeleteFeed")
-    public static let previousItem = NSNotification.Name("PreviousItem")
-    public static let nextItem = NSNotification.Name("NextItem")
 
 }
