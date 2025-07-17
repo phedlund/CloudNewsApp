9731bc549c4f9d8667b33bfb25e4787ccbbdd1a8
Peter Hedlund <support@pbh.dev>
2025-07-16 18:15:19 -0700

    Remove unused code

--- Shared/Publisher+Extension.swift
+++
@@ -1,9 +0,0 @@
-import Combine
-
-extension Publisher where Self.Failure == Never {
-    /// This is a workaround for the AsyncPublisher issue of WKWebView properties.
-    func bufferedValues() -> AsyncPublisher<Publishers.Buffer<Self>> {
-        self.buffer(size: 3, prefetch: .keepFull, whenFull: .dropOldest)
-            .values
-    }
-}
