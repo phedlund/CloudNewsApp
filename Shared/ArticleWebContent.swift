2bac545e49771a93ffda2daa09a1cff1e120bb70
Peter Hedlund <support@pbh.dev>
2025-08-03 15:35:31 -0700

    Cache css and update html

--- Shared/ArticleWebContent.swift
+++ Shared/ArticleWebContent.swift
@@ -61,17 +61,18 @@ struct ArticleWebContent: Identifiable {
                     let fileName = "summary_\(item.id)"
 
                     let htmlTemplate = """
-                    <?xml version="1.0" encoding="utf-8"?>
-                    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-                    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-                    <html xmlns="http://www.w3.org/1999/xhtml">
+                    <!DOCTYPE html>
+                    <html>
                         <head>
+                        <meta charset="utf-8">
                         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
                             <title>
                                 \(title)
                             </title>
-                        <style>\(updateCssVariables())</style>
-                        <link rel="stylesheet" href="\(String.cssPath)" media="all">
+                            <style>
+                                \(updateCssVariables())
+                                \(CssProvider.shared.css())
+                            </style>
                         <base href="\(baseString)">
                         </head>
                         <body>
